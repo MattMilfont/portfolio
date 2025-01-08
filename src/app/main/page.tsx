8f3b5da1d8b763fb5fc738f64365ecf7096f20c5
MattMilfont <matheusnmilfontc@gmail.com>
2025-01-08 19:53:39 -0300

    [Feature] Adicionando comentários à página inicial

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,29 +1,40 @@
+//Usando a renderização da página toda no cliente
 "use client";
 
+//Importando o hook de estado do react
 import { useState, useEffect } from "react";
+
+//Importando o hook de roteamento do next
 import { useRouter } from "next/navigation";
 
+//Importando o Header padrão dos componentes
 import Header from "@/components/header";
 
+//Função Principal que carrega a Página Principal
 export default function MainPage() {
+  //Declarando os estados que armazenam as variações de valores
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  const router = useRouter(); 
+  //Declarando o roteador para usar na página
+  const router = useRouter();
 
+  //Verifica a sessão do usuário
   const checkAuthentication = async () => {
-    const sessionKey = localStorage.getItem("sessionKey");  
-    
+    const sessionKey = localStorage.getItem("sessionKey");
+
     if (!sessionKey) {
       router.push("/");
     } else {
       setIsAuthenticated(true);
     }
   };
-  
-  useEffect(() => {
-      checkAuthentication();  
-  });
 
+  //Chama a função para ser executada no início da função
+  useEffect(() => {
+    checkAuthentication();
+  });
+  
+  //Parte visual da Página
   return (
     <div>
       <Header title="Mojave Express" />
