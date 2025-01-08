709b11aa177cecfe3d464aa204bb4b5c2052bd9e
MattMilfont <matheusnmilfontc@gmail.com>
2025-01-08 19:50:13 -0300

    [Feature] Comentando a página de Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,61 @@
+//Usando a renderização da página toda no cliente
 "use client";
 
+//Importando o hook de estado do react
 import { useState } from "react";
+
+//Importando o hook de roteamento do next
 import { useRouter } from "next/navigation";
+
+//Importando a autenticação do usuário pelo serviço de usuário
 import { authentication } from "@/services/UserService";
 
+//Função Principal que carrega a Página de Login
 export default function LoginPage() {
+
+  //Declarando os estados que armazenam as variações de valores
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  //Declarando o roteador para usar na página
   const router = useRouter();
 
+  //Função que executa o login e consulta o serviço de autenticação
   const handleLogin = async () => {
+
+    //Mudanças de estado para login
     setIsLoading(true);
     setMessage(null);
     setError(null);
 
     try {
+      //Consulta o método de autenticação do usuário
       const response = await authentication(email, password);
+
+      //Verifica se aconteceu uma resposta válida
       if (!response.ok) {
+        //Tratativa de erro
         const errorData = await response.json();
         throw new Error(errorData.error || "Erro desconhecido");
       }
 
+      //Transforma a resposta em um objeto JSON
       const data = await response.json();
+
+      //Define a mensagem no estado
       setMessage(data.message);
+
+      //Define a sessão
       localStorage.setItem('sessionKey', data.sessionKey); 
 
+      //Redireciona para a rota main
       router.push("/main");
     } catch (err: unknown) {
+      
+      //Tratativa de erro
       if (err instanceof Error) {
         setError(err.message);
       } else {
@@ -39,7 +65,8 @@ export default function LoginPage() {
       setIsLoading(false);
     }
   };
-
+  
+  //Parte visual da Página
   return (
     <div className="container mt-5">
       <div className="row">
